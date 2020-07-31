afbc3de37483e4b791d82d9f36620e7de9954672
Rodrigo_LPA <rodrigolpa@protonmail.com>
2020-07-31 17:08:46 +0200

    first runs at baseline model

--- models/baseline_CNN_LSTM/train.py
+++ models/baseline_CNN_LSTM/train.py
@@ -20,8 +20,8 @@ def train():
     )
 
     train_loader, dataset = get_loader(
-        root_folder="flickr8k/images",
-        annotation_file="flickr8k/captions.txt",
+        root_folder="data/flickr8k/images",
+        annotation_file="data/flickr8k/tiny_captions.txt",
         transform=transform,
         num_workers=2,
     )
@@ -33,12 +33,12 @@ def train():
     train_CNN = False
 
     # Hyperparameters
-    embed_size = 256
-    hidden_size = 256
+    embed_size = 5
+    hidden_size = 5
     vocab_size = len(dataset.vocab)
     num_layers = 1
     learning_rate = 3e-4
-    num_epochs = 5
+    num_epochs = 1
 
     # for tensorboard
     writer = SummaryWriter("runs/flickr")
@@ -49,12 +49,6 @@ def train():
     criterion = nn.CrossEntropyLoss(ignore_index=dataset.vocab.stoi["<PAD>"])
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    # Only finetune the CNN
-    for name, param in model.encoderCNN.inception.named_parameters():
-        if "fc.weight" in name or "fc.bias" in name:
-            param.requires_grad = True
-        else:
-            param.requires_grad = train_CNN
 
     if load_model:
         step = load_checkpoint(torch.load("my_checkpoint.pth.tar"), model, optimizer)
