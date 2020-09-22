bc16ba73adbd426a33ae8b0e31fe974d8629357f
Rodrigo_LPA <rodrigolpa@protonmail.com>
2020-09-22 19:21:03 +0200

    tiny updates

---
+++ models/caption_generator/retrieval.py
@@ -0,0 +1,17 @@
+import os
+os.getcwd()
+
+
+#load model
+
+#load level to evaluate
+
+#select target image and its golden caption
+
+#for every image in the level generate caption
+
+#get pair-wise BLEU scores
+
+#return argmax
+
+#evaluate if correct or not
